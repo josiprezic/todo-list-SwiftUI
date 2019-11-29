c96634f34c2c4252e8528c6342328095446ff4af
josiprezic <josip@feelmorelabs.com>
2019-11-29 13:22:05 +0100

    Initial Commit

---
+++ TodoList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TodoList
+//
+//  Created by Josip Rezic on 29/11/2019.
+//  Copyright © 2019 feelmorelabs. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
