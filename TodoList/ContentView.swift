b44a5f3646cc791ec2ff33bf6e95e372717c0d49
josiprezic <josip@feelmorelabs.com>
2019-11-29 18:09:29 +0100

    Add TaskView

--- TodoList/ContentView.swift
+++ TodoList/ContentView.swift
@@ -13,66 +13,100 @@ struct Day: Identifiable {
     let text: String
 }
 
+struct Task: Identifiable {
+    let id = UUID()
+    let title: String
+    let description: String = ""
+}
+
 struct ContentView: View {
-    
-    
     
     var days: [Day] = [
         Day(text:"Monday"),
+        Day(text:"Tuesday"),
+        Day(text:"Wednesday"),
+        Day(text:"Thursday"),
+        Day(text:"Friday"),
+        Day(text:"Saturday"),
+        Day(text:"Sunday"),
         Day(text:"Monday"),
-        Day(text:"Monday"),
-        Day(text:"Monday"),
-        Day(text:"Monday"),
-        Day(text:"Monday"),
-        Day(text:"Monday"),
-        Day(text:"Monday"),
-        Day(text:"Monday"),
-        Day(text:"Monday"),
-        Day(text:"Monday"),
-        Day(text:"Monday"),
-        Day(text:"Monday"),
-        Day(text:"Monday"),
-        Day(text:"Monday"),
-        Day(text:"Monday"),
-        Day(text:"Monday"),
-        Day(text:"Monday"),
-        Day(text:"Monday")
+        Day(text:"Tuesday"),
+    ]
+    
+    var tasks: [Task] = [
+        Task(title: "Task 1"),
+        Task(title: "Task 2"),
+        Task(title: "Task 3"),
+        Task(title: "Task 4"),
+        Task(title: "Task 5"),
+        Task(title: "Task 6"),
+        Task(title: "Task 7"),
+        Task(title: "Task 8"),
+        Task(title: "Task 9"),
+        Task(title: "Task 10"),
+        Task(title: "Task 11"),
+        Task(title: "Task 12"),
+        Task(title: "Task 13"),
+        Task(title: "Task 14"),
+        Task(title: "Task 15"),
+        Task(title: "Task 16"),
+        Task(title: "Task 17"),
+        Task(title: "Task 18"),
+        Task(title: "Task 19")
     ]
     
     var body: some View {
         NavigationView {
             List {
-                ScrollView(.horizontal, showsIndicators: true) {
+                Text("Stack title")
+                ScrollView(.horizontal, showsIndicators: false) {
                     VStack(alignment: .leading) {
-                        Text("Stack title")
                         HStack {
-                            Text("ScrollView").frame(width: 100, height: 180).background(Color.blue)
-                            Text("ScrollView").frame(width: 100, height: 180).background(Color.blue)
-                            Text("ScrollView").frame(width: 100, height: 180).background(Color.blue)
-                            Text("ScrollView").frame(width: 100, height: 180).background(Color.blue)
-                            Text("ScrollView").frame(width: 100, height: 180).background(Color.blue)
-                            Text("ScrollView").frame(width: 100, height: 180).background(Color.blue)
-                            Text("ScrollView").frame(width: 100, height: 180).background(Color.blue)
-                            Text("ScrollView").frame(width: 100, height: 180).background(Color.blue)
-                            Text("ScrollView").frame(width: 100, height: 180).background(Color.blue)
+                            ForEach(days) { day in
+                                Button(action: {
+                                    print("TEST")
+                                }) {
+                                    DayView(day: day)
+                                }
+                                
+                            }
                         }
                     }
                 }
-                    
                 .frame(height: 200.0)
-            
                 .background(Color.green)
                 
                 
-                ForEach(days) { day in
+                ForEach(tasks) { task in
                     NavigationLink(destination: DetailView()) {
-                        Text(day.text)
+                        TaskView(task: task)
                     }
                 }
             }
             .navigationBarTitle(Text("Todo list"))
-            .padding(0)
+            .padding(.leading, -10)
+            .padding(.trailing, -10)
         }
+    }
+}
+
+struct TaskView: View {
+    
+    let task: Task
+    
+    var body: some View {
+        Text(task.title)
+    }
+}
+
+struct DayView: View {
+    let day: Day
+    
+    var body: some View {
+        Text(day.text)
+            .frame(width: 100, height: 180)
+            .background(Color.blue)
+            .foregroundColor(Color.red)
     }
 }
 
