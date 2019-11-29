d902217f2ac6ab44f54d9b1c48edafe02b161c67
josiprezic <josip@feelmorelabs.com>
2019-11-29 17:41:56 +0100

    Update ContentView, add DetailView

--- TodoList/ContentView.swift
+++ TodoList/ContentView.swift
@@ -8,14 +8,84 @@
 
 import SwiftUI
 
+struct Day: Identifiable {
+    let id = UUID()
+    let text: String
+}
+
 struct ContentView: View {
+    
+    
+    
+    var days: [Day] = [
+        Day(text:"Monday"),
+        Day(text:"Monday"),
+        Day(text:"Monday"),
+        Day(text:"Monday"),
+        Day(text:"Monday"),
+        Day(text:"Monday"),
+        Day(text:"Monday"),
+        Day(text:"Monday"),
+        Day(text:"Monday"),
+        Day(text:"Monday"),
+        Day(text:"Monday"),
+        Day(text:"Monday"),
+        Day(text:"Monday"),
+        Day(text:"Monday"),
+        Day(text:"Monday"),
+        Day(text:"Monday"),
+        Day(text:"Monday"),
+        Day(text:"Monday"),
+        Day(text:"Monday")
+    ]
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            List {
+                ScrollView(.horizontal, showsIndicators: true) {
+                    VStack(alignment: .leading) {
+                        Text("Stack title")
+                        HStack {
+                            Text("ScrollView").frame(width: 100, height: 180).background(Color.blue)
+                            Text("ScrollView").frame(width: 100, height: 180).background(Color.blue)
+                            Text("ScrollView").frame(width: 100, height: 180).background(Color.blue)
+                            Text("ScrollView").frame(width: 100, height: 180).background(Color.blue)
+                            Text("ScrollView").frame(width: 100, height: 180).background(Color.blue)
+                            Text("ScrollView").frame(width: 100, height: 180).background(Color.blue)
+                            Text("ScrollView").frame(width: 100, height: 180).background(Color.blue)
+                            Text("ScrollView").frame(width: 100, height: 180).background(Color.blue)
+                            Text("ScrollView").frame(width: 100, height: 180).background(Color.blue)
+                        }
+                    }
+                }
+                    
+                .frame(height: 200.0)
+            
+                .background(Color.green)
+                
+                
+                ForEach(days) { day in
+                    NavigationLink(destination: DetailView()) {
+                        Text(day.text)
+                    }
+                }
+            }
+            .navigationBarTitle(Text("Todo list"))
+            .padding(0)
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(days: [
+            Day(text:"Monday"),
+            Day(text:"Monday"),
+            Day(text:"Monday"),
+            Day(text:"Monday"),
+            Day(text:"Monday"),
+            Day(text:"Monday"),
+            Day(text:"Monday")
+        ])
     }
 }
---
+++ TodoList/DetailView.swift
@@ -0,0 +1,22 @@
+//
+//  DetailView.swift
+//  TodoList
+//
+//  Created by Josip Rezic on 29/11/2019.
+//  Copyright © 2019 feelmorelabs. All rights reserved.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .background(Color.green)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView()
+    }
+}
